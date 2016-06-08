8c789110e0cee39feee8ac72949cfddcf944b037
shendapeng <shendapeng@yuantutech.com>
2016-06-08 17:33:40 +0800

    “schema”

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -42,37 +42,16 @@ function callApi(endpoint, schema) {
     })
 }
 
-// We use this Normalizr schemas to transform API responses from a nested form
-// to a flat form where repos and users are placed in `entities`, and nested
-// JSON objects are replaced with their IDs. This is very convenient for
-// consumption by reducers, because we can easily build a normalized tree
-// and keep it updated as we fetch more data.
 
-// Read more about Normalizr: https://github.com/paularmstrong/normalizr
-
-// GitHub's API may return results with uppercase letters while the query
-// doesn't contain any. For example, "someuser" could result in "SomeUser"
-// leading to a frozen UI as it wouldn't find "someuser" in the entities.
-// That's why we're forcing lower cases down there.
-
-const userSchema = new Schema('users', {
-  idAttribute: user => user.login.toLowerCase()
+const newsSchema = new Schema('news', {
+  idAttribute: news => news.type.toLowerCase()
 })
 
-const repoSchema = new Schema('repos', {
-  idAttribute: repo => repo.fullName.toLowerCase()
-})
-
-repoSchema.define({
-  owner: userSchema
-})
 
 // Schemas for Github API responses.
 export const Schemas = {
-  USER: userSchema,
-  USER_ARRAY: arrayOf(userSchema),
-  REPO: repoSchema,
-  REPO_ARRAY: arrayOf(repoSchema)
+  NEWS: newsSchema,
+  NEWS_ARRAY: arrayOf(newsSchema),
 }
 
 // Action key that carries API call info interpreted by this Redux middleware.
